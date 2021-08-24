172344509380d3cbee609b35a492580558b86724
Vladislav Likhachev <mymindissharp@gmail.com>
2021-08-24 08:33:07 +0300

    Добавил калькуляторы

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Ints intsCalc = new IntsCalculator();
+        System.out.println(intsCalc.sum(2, 2));
+        System.out.println(intsCalc.sum(10, 22));
+        System.out.println(intsCalc.mult(12, 12));
+        System.out.println(intsCalc.pow(2, 10));
     }
 }
